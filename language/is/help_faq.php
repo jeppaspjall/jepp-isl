a1346988a9e8f4aa0aa8c21c5da921fca35c1230
Árni Björnsson <jarni@jarni.net>
2015-05-28 11:16:43 +0000

    Byrjað á faq

--- language/is/help_faq.php
+++ language/is/help_faq.php
@@ -33,27 +33,27 @@ if (!defined('IN_PHPBB'))
 $help = array(
 	array(
 		0 => '--',
-		1 => 'Login and Registration Issues'
+		1 => 'Nýskráningar og innskráningarvandamál'
 	),
 	array(
-		0 => 'Why do I need to register?',
-		1 => 'You may not have to, it is up to the administrator of the board as to whether you need to register in order to post messages. However; registration will give you access to additional features not available to guest users such as definable avatar images, private messaging, emailing of fellow users, usergroup subscription, etc. It only takes a few moments to register so it is recommended you do so.'
+		0 => 'Hvers vegna þarf ég að skrá mig á spjallið?',
+		1 => 'Þú þarft þess ekki endilega. Þú getur skoðað og lesið spjallið án þess að skrá þig, en til þess að taka þátt í umræðum og nýta þér aðra eiginlega spjallborðsins þarftu að skrá þig. Það er mjög fljótlegt að skrá nýjan notanda, svo við mælum með því að þú gerir það.'
 	),
 	array(
-		0 => 'What is COPPA?',
-		1 => 'COPPA, or the Children’s Online Privacy Protection Act of 1998, is a law in the United States requiring websites which can potentially collect information from minors under the age of 13 to have written parental consent or some other method of legal guardian acknowledgment, allowing the collection of personally identifiable information from a minor under the age of 13. If you are unsure if this applies to you as someone trying to register or to the website you are trying to register on, contact legal counsel for assistance. Please note that phpBB Limited and the owner’s of this board cannot provide legal advice and is not a point of contact for legal concerns of any kind, except as outlined in question “Who do I contact about abusive and/or legal matters related to this board?”.',
+		0 => 'Hvað er COPPA?',
+		1 => 'COPPA stendur fyrir "Children’s Online Privacy Protection Act of 1998" eru bandarísk lög sem eiga ekki við hér.',
 	),
 	array(
-		0 => 'Why can’t I register?',
-		1 => 'It is possible a board administrator has disabled registration to prevent new visitors from signing up. A board administrator could have also banned your IP address or disallowed the username you are attempting to register. Contact a board administrator for assistance.',
+		0 => 'Hvers vegna get ég ekki nýskráð mig?',
+		1 => 'Mögulega hafa stjórnendur gert nýskráningu notanda óvirka. Einnig gæti verið að IP tala þín eða notendanafn sé á bannlista. Hafðu samband við stjórnanda ef þú þarft hjálp.',
 	),
 	array(
-		0 => 'I registered but cannot login!',
-		1 => 'First, check your username and password. If they are correct, then one of two things may have happened. If COPPA support is enabled and you specified being under 13 years old during registration, you will have to follow the instructions you received. Some boards will also require new registrations to be activated, either by yourself or by an administrator before you can logon; this information was present during registration. If you were sent an email, follow the instructions. If you did not receive an email, you may have provided an incorrect email address or the email may have been picked up by a spam filer. If you are sure the email address you provided is correct, try contacting an administrator.'
+		0 => 'Ég nýskráði notanda en get ekki skráð mig inn!',
+		1 => 'Byrjaðu á því að athuga hvort notendanafn og lykilorð séu rétt. Ef þau eru rétt gæti eftirfarandi hafa gerst. Ef COPPA er virkt og þú undir 13 ára aldri við nýskráningu, þá ættir þú að hafa fengið sendar leiðbeiningar sem þú þarft að fylgja. Sum spjallborð krefjast þess að nýir notendur séu staðfestir, annað hvort af þeim sjálfum eða stjórnendum, áður en þú getur skráð þig inn. Þær upplýsingar komu fram við nýskráningu. Ef þú fékkst sendan tölvupóst, fylgdu fyrirmælunum sem þar eru að finna. Ef þú fékkst ekki tölvupóst gæti verið að þú hafi skráð rangt tölvupóstfang eða að tölvupósturinn hafi lent í ruslasíu. Ef þú ert viss um að tölvupóstfangið sem þú skráðir hafi verið rétt, hafðu samband við stjórnanda.'
 	),
 	array(
-		0 => 'Why can’t I login?',
-		1 => 'There are several reasons why this could occur. First, ensure your username and password are correct. If they are, contact a board administrator to make sure you haven’t been banned. It is also possible the website owner has a configuration error on their end, and they would need to fix it.',
+		0 => 'Hvers vegna get ég ekki skráð mig inn?',
+		1 => 'Það gætu verið nokkrar ástæður. Í fyrsta lagi, gakktu úr skugga um að notendanafn og lykilorð séu rétt. Ef þau eru rétt, hafðu samband við stjórnanda ef ske kynni að notandi þinn hefi verið bannaður. Það gæti einnig verið villa á okkar enda sem við þurfum að laga!',
 	),
 	array(
 		0 => 'I registered in the past but cannot login any more?!',
